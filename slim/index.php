d5873d3e7a923fc7857afd304182a53444dd6c37
ezequiel <alcayaga147147@gmail.com>
2026-05-10 22:34:01 -0300

    eliminado de endpoint duplicados

--- slim/index.php
+++ slim/index.php
@@ -59,19 +59,10 @@ $app->add(function ($request, $handler) {
 // Rutas de usuario y autenticación
 $app->post('/users', [UserController::class, 'registrar']);
 $app->post('/login', [UserController::class, 'login']);
-$app->post('/logout', [UserController::class, 'logout'])->add(new AuthMiddleware());
 
 $app->get('/assets', [AssetController::class, 'listar']);
-$app->put('/assets', [AssetController::class, 'actualizarPrecio'])->add(new AuthMiddleware());
+
 $app->get('/assets/{asset_id}/history/{quantity}', [AssetController::class, 'consultarHistorial']);
-
-$app->get('/portfolio', [PortfolioController::class, 'obtenerPortafolio'])->add(new AuthMiddleware());
-$app->delete('/portfolio/{asset_id}', [PortfolioController::class, 'borrarPortafolio'])->add(new AuthMiddleware());
-
-$app->post('/trade/buy', [TradeController::class, 'buy'])->add(new AuthMiddleware());
-$app->post('/trade/sell', [TradeController::class, 'vender'])->add(new AuthMiddleware());
-
-$app->get('/transactions', [TransactionController::class, 'obtenerTransacciones'])->add(new AuthMiddleware());
 
 // Manejador para rutas no encontradas
 $errorMiddleware->setErrorHandler(
@@ -86,29 +77,22 @@ $errorMiddleware->setErrorHandler(
     }
 );
 
-// Grupo de rutas protegidas
-// $app->group('/api', function ($group) {
-//     $group->get('/test-auth', function ($request, $response) {
-//         $userId = $request->getAttribute('user_id');
-//         $response->getBody()->write(json_encode([
-//             "status" => "success",
-//             "message" => "Acceso concedido para el usuario ID: " . $userId
-//         ]));
-//         return $response->withHeader('Content-Type', 'application/json');
-//     });
+//Grupo de rutas protegidas
+$app->group('', function ($group) {
+    $group->get('/users/{user_id}', [UserController::class, 'verPerfil']);
+    $group->put('/users/{user_id}', [UserController::class, 'editarPerfil']);
+    $group->get('/users', [UserController::class, 'listarUsuarios']);
+    $group->post('/logout', [UserController::class, 'logout']);
 
-//     $group->get('/users/{user_id}', [UserController::class, 'verPerfil']);
-//     $group->put('/users/{user_id}', [UserController::class, 'editarPerfil']);
-//     $group->get('/users', [UserController::class, 'listarUsuarios']);
-//     $group->post('/logout', [UserController::class, 'logout']);
+    $group->get('/portfolio', [PortfolioController::class, 'obtenerPortafolio']);
+    $group->delete('/portfolio/{asset_id}', [PortfolioController::class, 'borrarPortafolio']);
 
-//     $group->get('/portfolio', [PortfolioController::class, 'obtenerPortafolio']);
-//     $group->delete('/portfolio/{asset_id}', [PortfolioController::class, 'borrarPortafolio']);
+    $group->post('/trade/buy', [TradeController::class, 'buy']);
+    $group->post('/trade/sell', [TradeController::class, 'vender']);
 
-//     $group->post('/trade/buy', [TradeController::class, 'buy']);
-//     $group->post('/trade/sell', [TradeController::class, 'vender']);
+    $group->get('/transactions', [TransactionController::class, 'obtenerTransacciones']);
 
-//     $group->get('/transactions', [TransactionController::class, 'obtenerTransacciones']);
-// })->add(new AuthMiddleware());
+    $group->put('/assets', [AssetController::class, 'actualizarPrecio']);
+})->add(new AuthMiddleware());
 
 $app->run();
