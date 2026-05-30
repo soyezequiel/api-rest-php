03b94f656f348e9f30ead1a36d1efafce694f856
Fabricio Gonzalez <fabricioxgonzalez@gmail.com>
2026-05-29 23:27:33 -0300

    Remove total_amount from transaction query in getHistorial method

--- slim/src/Models/Asset.php
+++ slim/src/Models/Asset.php
@@ -68,7 +68,7 @@ class Asset
         }
 
         $stmt = $db->prepare("
-            SELECT transaction_type, price_per_unit, total_amount, transaction_date FROM transactions
+            SELECT transaction_type, price_per_unit, transaction_date FROM transactions
             WHERE asset_id = ?
             ORDER BY transaction_date DESC
             LIMIT $quantity
